f7c6cbb1a65fdddd5c553942e4416c1ba3983dd5
rubenscpneto <rubenscpneto@hotmail.com>
2024-10-16 11:32:26 -0300

    second commit (button with alert)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}> My alert </Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
+    </div>
+  );
 }
 
 export default App;
